0f420b852e45da9cb6dac512706a944b7a2d2fe7
TShipley <taneymail@gmail.com>
2020-02-01 23:27:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,13 @@
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+def my_each # put argument(s) here
+  # code here
+end 
+def my_each(array)
+  i = 0
+
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+
+  array
 end
